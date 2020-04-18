6c7d065f78de421d1282cc5d48b523878a8caa1d
themikegit <rostovniki5@gmail.com>
2020-04-18 13:37:21 +0200

    Updates

---
+++ precache-manifest.1d45e652a6a637d9eccd0b51dcbb9b6a.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "caf0a7e55c895c9caa3cb899c09d22b0",
+    "url": "/todo_app/index.html"
+  },
+  {
+    "revision": "58c6de0e3500ff9838f8",
+    "url": "/todo_app/static/css/main.5ed51c52.chunk.css"
+  },
+  {
+    "revision": "15725fdcd3c836ad8860",
+    "url": "/todo_app/static/js/2.b342cdf8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo_app/static/js/2.b342cdf8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "58c6de0e3500ff9838f8",
+    "url": "/todo_app/static/js/main.c48fcb1f.chunk.js"
+  },
+  {
+    "revision": "8f9662681a69419789b2",
+    "url": "/todo_app/static/js/runtime-main.c4c997f9.js"
+  },
+  {
+    "revision": "f1008644d3f8030c0309bddaa0ad533d",
+    "url": "/todo_app/static/media/add.f1008644.svg"
+  },
+  {
+    "revision": "5f340c4d4aa9880301cae79b5aa5053a",
+    "url": "/todo_app/static/media/check.5f340c4d.svg"
+  },
+  {
+    "revision": "d1ac71d729453bf288f16aac73cd2461",
+    "url": "/todo_app/static/media/close.d1ac71d7.svg"
+  },
+  {
+    "revision": "11e8554a281971b5fc92c175a886ea7f",
+    "url": "/todo_app/static/media/restore.11e8554a.svg"
+  }
+]);
